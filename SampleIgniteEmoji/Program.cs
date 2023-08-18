c7946447272a20a547dd7022c2fa76b8363fe703
Mircea Militaru <mircea.militaru@druidai.com>
2023-08-18 17:48:31 +0300

    Add project files.

---
+++ SampleIgniteEmoji/Program.cs
@@ -0,0 +1,28 @@
+﻿using Apache.Ignite.Core;
+
+IIgnite ignite = Ignition.Start();
+
+Person firstPerson = new() { Id = Guid.NewGuid(), Name = "MrQ" };
+Apache.Ignite.Core.Cache.ICache<Guid, Person> cache = ignite.GetOrCreateCache<Guid, Person>(nameof(Person));
+cache.Put(firstPerson.Id, firstPerson);
+
+const int nbGetAndWrites = 8;
+// Get, Update, Put same record multiple times and add each time an Emoji
+for (int i = 0; i < nbGetAndWrites; i++)
+{
+    Person fromCache = cache.Get(firstPerson.Id);
+    Console.WriteLine($"Step {i} got Person {fromCache}");
+    fromCache.Name += $" [🐝]";
+    cache.Put(fromCache.Id, fromCache);
+}
+
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public override string ToString()
+    {
+        return $"[ Id={Id}; Name={Name} ]";
+    }
+}
